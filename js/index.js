231e0de303a21d1a0f6b15122e323d2571118273
Clement97712 <mj.capou@gmail.com>
2018-07-15 14:02:40 +0200

    no comment

---
+++ js/index.js
@@ -0,0 +1,138 @@
+var tablePicture = [
+  "montres_molles",
+  "chateau_noir",
+  "chambre",
+  "dernier",
+  "demoiselle_avignon"
+];
+var tableNomOeuvre = [
+  "<b>Salvador Dali: </b>Les montres molles",
+  "<b>Paul Cezane: </b>Chateau noir",
+  "<b>Van Gogh: </b>La chambre a coucher",
+  "<b>Leonard de Vinci: </b> La cene ou Le dernier Souper",
+  "<b>Pablo Picasso: </b>Les demoiselles d'avignon"
+];
+var tableComment = {
+  comment: [
+    ["<p><b>Luc: </b>...</p>"],
+    [
+      "<p><b>Nicolas: </b>Quel joie d'admirer une telle oeuvre d'art.</p>",
+      "<p><b>Antoine: </b>Magnifique!</p>",
+      "<p><b>Maria:</b> Je suis en admiration total dans la peinture de cette artiste.</p>",
+      "<p><b>Dzenifer:</b> Carpe diem de quoi</p>",
+      "<p><b>Nicolas: </b>Quel joie d'admirer une telle oeuvre d'art.</p>",
+      "<p><b>Antoine: </b>Magnifique!</p>",
+      "<p><b>Nicolas: </b>Quel joie d'admirer une telle oeuvre d'art.</p>",
+      "<p><b>Antoine: </b>Magnifique!</p>",
+      "<p><b>Maria:</b> Je suis en admiration total dans la peinture de cette artiste.</p>",
+      "<p><b>Dzenifer:</b> Carpe diem de quoi</p>",
+      "<p><b>Nicolas: </b>Quel joie d'admirer une telle oeuvre d'art.</p>",
+      "<p><b>Antoine: </b>Magnifique!</p>",
+      "<p><b>Maria:</b> Je suis en admiration total dans la peinture de cette artiste.</p>",
+      "<p><b>Dzenifer:</b> Carpe diem de quoi</p>",
+      "<p><b>Nicolas: </b>Quel joie d'admirer une telle oeuvre d'art.</p>",
+      "<p><b>Antoine: </b>Magnifique!</p>",
+      "<p><b>Maria:</b> Je suis en admiration total dans la peinture de cette artiste.</p>",
+      "<p><b>Dzenifer:</b> Carpe diem de quoi</p>"
+    ],
+    [
+      "<p><b>Hermann: </b>Comment la representation d'une simple chambre peut etre une oeuvre a plusieurs millions !!</p>",
+      "<p><b>Claude: </b>No comment, Admiré simplement!</p>"
+    ],
+    [
+      "<p><b>Jules: </b>Je ne suis pas une grand fan d'art!!</p><p><b>Theo: </b>C'est une chance immense que nous avons d'admirer ce chef-d'oeuvre.</p><p><b>Marie:</b>M'a coupé le souffre!!</p><p><b>Hortense:</b>La jonconde, L'homme de vitruse et meme cette toile me touche. La technique, le choix des couleurs, la texteure. Formidable!!!</p><p><b>Constance:</b> Quel beaute! on reconnait bien le genie de Leonard de vinci.</p>"
+    ],
+    [
+      "<p><b>Hermann:</b> Pablo, Pablo, Pablo Picasso!!</p>",
+      "<p><b>Claude:</b> La maniere de mettre en avant le corps des femmes est époustouflant. Il fallait de l'audace!! </p>",
+      "<p><b>Francoise:</b> Soit vous etes fan du cubisme ou pas</p>",
+      "<p><b>Clementine:</b> Cette toile!! Elle rapelle a quelle point la femme est synonymes de sensualite, de beaute. Nous savons donner de l'inspiration aux artistes.</p>"
+    ]
+  ]
+};
+
+var biographie = [
+  "<p><b>Nom: </b>Dali</p><p><b>Prenom: </b>Salvador</p><p><b>Nationalite: </b>Espagnole</p><p><b>Date de naissance: </b>11 mai 1904 a Figueras (Espagne)</p><p><b>Date de deces: </b>23 janvier 1989 a Figueras (Espagne)</p><p><b>Mouvement culturel: </b>Surrealisme</p><p><b>Description tableau: </b>La Persistance de la mémoire est un tableau surréaliste peint en 1931 par Salvador Dalí. C'est une huile sur toile connue dans le grand public sous le titre Les Montres molles et l'un des plus célèbres tableaux du peintre.Occupant le tiers supérieur de la toile, et de haut en bas, le tableau représente un ciel crépusculaire dominant une plage et la Méditerranée. L'horizon marin qui sert de point de fuite est le seul élément lumineux de la toile. La crique, les rochers et les falaises de Portlligat sont représentés à droite. Occupant le tiers supérieur de la toile, et de haut en bas, le tableau représente un ciel crépusculaire dominant une plage et la Méditerranée. L'horizon marin qui sert de point de fuite est le seul élément lumineux de la toile. La crique, les rochers et les falaises de Portlligat sont représentés à droite. Les deux tiers inférieurs de la toile sont occupés par la plage où se trouvent trois montres molles et une dure. Les couleurs dominantes sont froides. Des montres, l'une est suspendue à une branche « d'olivier12 », l'autre repose sur une forme blanche cadavérique allongée sur le sable qui n'est autre qu'une représentation déformée du Grand masturbateur. Enfin, les deux dernières montres reposent sur le bord d'un meuble situé à l'extérieur du champ de vision du spectateur et dont seul un angle est visible dans le coin inférieur gauche de l’œuvre12.Aucune de ces montres n'indique la même heure, même si les couleurs et la lumière sont celles du crépuscule. Des deux dernières montres, l'une est molle et est surmontée par une mouche - insecte volant et lié à un sentiment positif chez Dali. La dernière montre est la seule rigide et la seule peinte dans une couleur chaude, orangée, se démarquant nettement des montres molles aux tonalités bleutées froides. C'est une montre à gousset tournée sur son envers et masquant en conséquence l'heure. Elle est infestée de fourmis - insecte au contraire, lié à un sentiment d'horreur chez le peintre.</p>",
+  "<p><b>Nom: </b>Cezanne</p><p><b>Prenom: </b>Paul</p><p><b>Nationalite: </b>Francais</p><p><b>Date de naissance: </b>19 janvier 1839 a Aix en provence (France)</p><p><b>Date de deces: </b>22 octobre 1906 a Aix en provence (France) </p><p><b>Mouvement culturel: </b>Impressionnisme</p><p><b>D'autres oeuvres:</b> Les joueurs de cartes, Les grandes baigneuses, Le garcon au gilet rouges...</p><p><b>Description tableau: </b>Après s'être installé à Aix, en France, en 1899, Cézanne s'aventura quotidiennement dans le paysage provençal environnant à la recherche de sujets à peindre. Château Noir, un château néo-gothique récemment construit, conçu pour imiter les ruines âgées, l'a captivé. Il a représenté à plusieurs reprises cette structure et a également peint à partir de ses motifs, où il avait une vue imprenable sur la proximité Mont Sainte-Victoire, un autre sujet privilégié. Comme c'est souvent le cas pour les paysages réalisés tard dans sa carrière, Cæzanne a appliqué de la peinture épaisse dans de larges échantillons multicouches.</p>",
+  "<p><b>Nom: </b>van Gogh</p><p><b>Prenom: </b>Vincent</p><p><b>Nationalite: </b>Néerlandais</p><p><b>Date de naissance: </b>30 mars 1853 Groot Zundert (Pays-Bas)</p><p><b>Date de deces: </b>29 juillet 1890 a Auvers-sur-Oise (France) </p><p><b>Mouvement culturel: </b>Symbolisme</p><p><b>D'autres oeuvres: </b>Les mangeurs de pommes de terre, Les tournesols, La nuit étoilée...</p> <p><b>Description tableau: </b>Il réalise cette peinture en octobre 1888.L'objet dominant de la chambre est le lit : solide et simple, il suggère la chaleur, le confort et la sécurité. La plupart des autres objets (les chaises, les coussins et les tableaux) sont représentés par paires. Cette représentation contribue à donner une impression de tranquillité, d'ordre et de paix.Au-dessus du lit se trouvent le portrait du peintre Eugène Boch et le portrait de Paul-Eugène Milliet.Van Gogh réalisera deux autres versions de ce tableau. L'original ayant été abîmé, son frère Théo lui demanda d'en réaliser une copie avant que l'original ne soit restauré. Van Gogh peindra donc en 1889 la deuxième version de La Chambre de Van Gogh à Arles (sans toutefois chercher à faire une copie exacte du tableau de 1888).Encouragé par le résultat, Van Gogh réalisera une troisième version, plus petite (57 × 74 cm), qu'il offrira en cadeau à sa sœur (et non à sa belle-mère [ou mère] comme cela est souvent rapporté).</p>",
+  "<p><b>Nom: </b>de Vinci</p><p><b>Prenom: </b>Leonard</p><p><b>Nationalite: </b>Italien</p><p><b>Date de naissance: </b>15 avril 1452 a Vinci (Toscane, Italie) </p><p><b>Date de deces: </b>2 mai 1519 a Amboise (Touraine, France) </p><p><b>Mouvement culturel: </b>Renaissance</p><p><b>D'autres oeuvres: </b>La joconde, L'annonciation, L'homme de vitruve...</p><p><b>Description tableau: </b>Une grande fresque (460 par 880 cm) peinte par Léonard de Vinci entre 1495 et 1498 sur le mur de Santa Maria Delle Grazie à Milan, est l’un des tableaux les plus emblématiques de tous les temps, et certainement l’un des plus reconnaissables de De Vinci. C’est un exemple parfait de l’art chrétien de la Renaissance, qui illustre le verset 13 :21 de l’Evangile selon Saint Jean où Jésus, entouré de ses disciples, révèle que l’un d’entre eux va bientôt le trahir. Le tableau capture les réactions des apôtres de façon vive, montrant chacune de leurs personnalités en action. </p>",
+  "<p><b>Nom: </b>Picasso</p><p><b>Prenom: </b>Pablo</p><p><b>Nationalite: </b>Espagnole</p><p><b>Date de naissance: </b>25 octobre 1881 a Malaga (Espagne)</p><p><b>Date de deces: </b>8 avril 1973 a Mougins (France) </p><p><b>Mouvement culturel: </b>Cubisme, Surrealisme</p><p><b>D'autres oeuvres:</b> Guernica, Le Reve...</p><p><b>Description tableau: </b>Sur une scène, devant un rideau de théâtre, cinq femmes, partiellement nues, occupent la totalité du tableau. Au premier plan et au centre de ce tableau, se trouve une coupe de fruits dans laquelle sont posés un quartier de pastèque, une grappe de raisin ainsi qu'une pomme et une poire semblables. Ces cinq femmes sont peintes dans un camaïeu incarnat qui contraste avec le bleu, le blanc et le marron du rideau que la femme de gauche ouvre avec sa main. Les femmes du centre fixent le(s) spectateur(s) de leurs yeux exorbités. Si leur visage est dessiné de face, leur nez est dessiné de profil. La cinquième femme à droite est accroupie et nous tourne le dos. Le corps de ces femmes semble déformé, représenté à la fois de profil, de trois quarts, de face et de dos, avec des contours faits de segments de droites et d'angles vifs.</p>"
+];
+
+var boutonright = document.getElementById("right");
+var picture = document.getElementById("picture");
+var position = tablePicture.indexOf(picture.alt);
+var listComment = document.getElementById("comments");
+var taille = tablePicture.length - 1;
+var isNomOeuvre = document.getElementById("oeuvre");
+var donnees = document.getElementById("isdata");
+var toggle = true;
+var resume = document.getElementById("summary");
+
+document.getElementById("summary").style.display = "none";
+listComment.innerHTML = tableComment.comment[position].join(" ");
+resume.innerHTML = biographie[position];
+
+//console.log(taille);
+//console.log("position actuelle:" + position);
+//var convert = position.toString();
+//console.log(typeof convert);
+//console.log(tableComment.comment[1]);
+
+function next() {
+  if (position === taille) {
+    position = 0;
+    console.log("element : " + position);
+    isChoose(position);
+  } else {
+    position++;
+    console.log("element : " + position);
+    isChoose(position);
+  }
+}
+
+function previous() {
+  if (position == 0) {
+    position = taille;
+    console.log("previous : " + position);
+    isChoose(position);
+  } else {
+    position--;
+    console.log("previous :" + position);
+    isChoose(position);
+  }
+}
+
+function isChoose(valeur) {
+  picture.src = "images/" + tablePicture[valeur] + ".jpg";
+  picture.alt = tablePicture[valeur];
+  listComment.innerHTML = tableComment.comment[valeur].join(" ");
+  isNomOeuvre.innerHTML = tableNomOeuvre[valeur];
+  resume.innerHTML = biographie[position];
+}
+
+function affichage() {
+  if (toggle) {
+    document.getElementById("summary").style.display = "block";
+    console.log(document.getElementById("action").className);
+    document.getElementById("action").className = "inus";
+    toggle = false;
+  } else {
+    document.getElementById("summary").style.display = "none";
+    toggle = true;
+  }
+}
+
+function sendData() {
+  console.log(donnees);
+  if (donnees !== "") {
+    console.log(donnees.value);
+    tableComment.comment[position].push(
+      "<p><b>Lucien: </b>" + donnees.value + "</p>"
+    );
+    listComment.innerHTML += "<p><b>Lucien: </b>" + donnees.value + "</p>";
+    donnees.value = "";
+  } else {
+    console.log("vide");
+  }
+}
